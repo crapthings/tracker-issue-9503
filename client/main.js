833f4a63e8053ac3a2b9258d237f69c5e9b33a3b
crapthings <crapthings@gmail.com>
2017-12-28 17:22:00 +0800

    issue

--- client/main.js
+++ client/main.js
@@ -1,14 +1,83 @@
-a = Tracker.autorun(() => {
-  console.log('level 1')
-  console.log('add collection reactive source', Test.find().count())
-  Tracker.nonreactive(() => {
-    console.log('level 2')
-    console.log('ts is', Session.get('ts'))
-    Tracker.autorun(c => {
-        console.log('level 3')
-        console.log('should i print times?', Session.get('ts'))
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { mount } from 'react-mounter'
+
+class LayoutA extends Component {
+  componentWillMount() {
+    console.log('layout a did mount', 1)
+  }
+
+  componentWillUnmount() {
+    console.log('layout a will unmount', 1)
+  }
+
+  render() {
+    return <div>
+      {this.props.children()}
+    </div>
+  }
+}
+
+class A extends Component {
+  componentWillMount() {
+    console.log('component a did mount', 2)
+  }
+
+  componentWillUnmount() {
+    console.log('component a will unmount', 2)
+  }
+
+  render() {
+    return <div>A</div>
+  }
+}
+
+FlowRouter.route('/', {
+  action() {
+    mount(LayoutA, {
+      children() {
+        return <A />
+      }
     })
-  })
+  }
 })
 
-Session.set('ts', Date.now())
+class LayoutB extends Component {
+  componentWillMount() {
+    console.log('layout b did mount', 1)
+  }
+
+  componentWillUnmount() {
+    console.log('layout b will unmount', 1)
+  }
+
+  render() {
+    return <div>
+      {this.props.children()}
+    </div>
+  }
+}
+
+class B extends Component {
+  componentWillMount() {
+    console.log('component b did mount', 2)
+  }
+
+  componentWillUnmount() {
+    console.log('component b will unmount', 2)
+  }
+
+  render() {
+    return <div>B</div>
+  }
+}
+
+FlowRouter.route('/b', {
+  action() {
+    mount(LayoutB, {
+      children() {
+        return <B />
+      }
+    })
+  }
+})
--- server/main.js
+++ server/main.js
@@ -1,10 +1,24 @@
 import _ from 'lodash'
 import faker from 'faker'
 
-Test.remove({})
+Test1.remove({})
+Test2.remove({})
 
-const dummies = _.times(5000, idx => ({
+const test1 = _.times(5000, idx => ({
   content: faker.lorem.sentences(),
 }))
 
-Test.batchInsert(dummies)
+const test2 = _.times(5000, idx => ({
+  name: faker.name.findName(),
+}))
+
+Test1.batchInsert(test1)
+Test2.batchInsert(test2)
+
+Meteor.publish('test1', function () {
+  return Test1.find()
+})
+
+Meteor.publish('test2', function () {
+  return Test2.find()
+})
