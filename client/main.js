d6657fa8857c68b4827a043bd746d67757a6d1cc
crapthings <crapthings@gmail.com>
2017-12-29 10:30:19 +0800

    minor update

--- client/main.js
+++ client/main.js
@@ -1,14 +1,17 @@
 a = Tracker.autorun(() => {
+  let computation
   console.log('level 1')
   console.log('add collection reactive source', Test.find().count())
   Tracker.nonreactive(() => {
     console.log('level 2')
     console.log('ts is', Session.get('ts'))
-    Tracker.autorun(c => {
+    computation = Tracker.autorun(c => {
         console.log('level 3')
         console.log('should i print times?', Session.get('ts'))
     })
   })
-})
 
-Session.set('ts', Date.now())
+  Tracker.onInvalidate(() => {
+    computation.stop()
+  })
+})
